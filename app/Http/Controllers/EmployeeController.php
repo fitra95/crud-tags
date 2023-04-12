aebfaa3070f7c01c62b133d4ca72599fdb6cb611
unknown <fitrahulanwar95@gmail.com>
2023-04-12 13:01:30 +0700

    crud tags

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index(){
+
+    $data = Employee::all();
+    return view('datapegawai', compact('data'));
+    } 
+
+    public function tambahpegawai(){
+        return view('tambahdata');
+    }
+
+    public function insertdata(Request $request){
+        //dd($request->all());
+        Employee::create($request->all());
+        return redirect()->route('pegawai')->with('success','Data Berhasil Dibuat');
+    }
+
+    public function tampilkandata($id){
+
+        $data = Employee::find($id);
+        //dd($data);
+        return view('tampilkandata', compact('data'));
+    }
+
+    public function updatedata(Request $request, $id){
+        $data = Employee::find($id);
+        $data->update($request->all());
+        return redirect()->route('pegawai')->with('success','Data Berhasil Di Update');
+    }
+
+    public function delete($id){
+        $data = Employee::find($id);
+        $data->delete();
+        return redirect()->route('pegawai')->with('success','Data Berhasil Di Hapus');
+    }
+}
---
+++ resources/views/tampilkandata.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud Laravel 8</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 class="text-center mb-4">Edit Tambah Data Pegawai</h1>
+
+     <div class="container">
+    
+        <div class="row justify-content-center">
+          <div class="col-6">
+           <div class="card">
+            <div class="card-body">
+          <form action="/updatedata/{{ $data->id }}" method="POST" enctype="multipart/form-data">
+            @csrf
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+    <input type="text" name ="nama" class="form-control" id="exampleInputEmail1"
+    aria-describedby="emailHelp" value="{{ $data->nama }}">
+  </div>
+  
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Slug</label>
+    <input type="text" name ="slug" class="form-control" id="exampleInputEmail1"
+    aria-describedby="emailHelp" value="{{ $data->slug }}">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+ </div>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
